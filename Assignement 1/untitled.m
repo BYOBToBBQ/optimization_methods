eacf995c94ff2c2f321711ef60ae7d250604f1f7
tomas <girotomas@gmail.com>
2018-09-25 15:51:45 +0200

    tomas

---
+++ Assignement 1/untitled.m
@@ -0,0 +1,39 @@
+S=100;
+K=100;
+T=1.5;
+r=0.04;
+
+sigma=1;
+while abs(f(S,K,T,sigma,r)-10.78)>0.01
+    sigma=x1(S,K,T,sigma,r);
+end
+fprintf("The implied volatility is %.4f \n",sigma)
+
+
+
+function res=f(S,K,T,sig,r)
+res=S*normcdf(d1(S,K,T,sig,r))-K.*exp(-r*T).*normcdf(d2(S,K,T,sig,r));
+end
+
+
+function res=vega(S,K,T,sig,r)
+res=S*exp(-d1(S,K,T,sig,r).^2/2)./(sqrt(2*pi)).*sqrt(T);
+end
+
+function res=d1(S,K,T,sig,r)
+res=(log(S/K)+(r+0.5.*sig.^2)*T)./(sig.*sqrt(T));
+end
+
+function res=d2(S,K,T,sig,r)
+res=d1(S,K,T,sig,r)-sig.*sqrt(T);
+end
+
+%Computes one iteration of the Newton method
+function res=x1(S,K,T,sig,r)
+res=sig-(f(S,K,T,sig,r)-10.78)./vega(S,K,T,sig,r);
+end
+
+
+
+
+
---
+++ Assignement 1/untitled2.m
@@ -0,0 +1,28 @@
+[X,Y] = meshgrid(-2:.2:2); 
+Z = (X-2)^4+(X-2*Y)^2;
+surf(X,Y,Z)
+
+
+x=[3;3];
+n=0;
+while abs(x(1))>1e-3 &&  abs(x(2))>1e-3
+    n=n+1;
+    x=iterate(x)
+end
+
+function res=iterate(x)
+res=x-pinv(hessian(x))*(gradient(x));
+end
+
+function res=f(x1,x2)
+res=(x1-2)^4+(x1-2*x2)^2;
+end
+
+function res=hessian(x)
+x1=x(1);
+res=[12*(x1-2)^2+2, -4; -4 ,8]
+end
+
+function res=gradient(x)
+res=[ 4*(x(1)-2)^3+2*(x(1)-2*x(2)); -4*(x(1)-2*x(2)) ];
+end
