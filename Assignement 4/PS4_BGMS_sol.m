d98bee8dbd79a67d6d16418d6a83c428df250b39
IvanS <ivan.schonenberger@epfl.ch>
2018-10-25 19:51:20 +0200

    final

--- Assignement 4/PS4_BGMS_sol.m
+++ Assignement 4/PS4_BGMS_sol.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 %Define parameters
 beta=0.99;
-A=4;
+A=8;
 B=4;
 %+1 to get actual number of points on grid
 NumPoints =999;
@@ -16,21 +16,26 @@ X = x_lo:step:x_hi;
 n=length(X);
 %NumPoints+1, it's the actual number of points on the grid
 
+%arrange our function as a vector since number of grid points is countable
 V_0 = -(A+B)*X.^2;
 M = zeros(n, n);
 max_stop =1;
 while max_stop > 0.00001
     V_next = zeros(1,n);
+    %brute force test all possible values of the function
     for i=1:n
         for j=1:n
             M(i,j)=-A*(X(i)-X(j))^2-B*X(i)^2+beta*V_0(j);
         end
     end
     
+    %select only the maximum along x(j). i.e select the max of each row to
+    %get the function
     for i=1:n
         V_next(i)= max(M(i,:));
     end
     
+    %check convergence condition
     max_stop = max(abs(V_next-V_0));
     V_0=V_next;
 end
